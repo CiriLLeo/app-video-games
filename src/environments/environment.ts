a370dd2b6b6acae651c53069fbfba9a96f2d5f30
leonardo <leo.cirillo92@gmail.com>
2024-08-01 10:40:19 +0200

    prova per nascondere apikey

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: false,
-  BASE_URL: 'https://rawg-video-games-database.p.rapidapi.com'
+  BASE_URL: 'https://rawg-video-games-database.p.rapidapi.com',
+  rapidapiKey: '88a999c8d6msh5782e0f0696f296p1d2609jsn6f573e8b5948',
+  rapidapiHost: 'rawg-video-games-database.p.rapidapi.com',
+  apiKey: 'aa89d4f042ae4ab8904194984cb2e317'
 };
